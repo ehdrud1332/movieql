2e3d06bc0eaff790893966f1893950babd45b207
castle.jaemin <ehdrud1332@gmail.com>
2021-03-09 14:41:18 +0900

    2.3 create first Mutation

--- db.js
+++ db.js
@@ -1,52 +1,53 @@
 //Resolver 는 query를 resolve(해결) 한다.
-export const people = [
+let movies = [
     {
-        id: "1",
-        name: 'seong',
-        age: 25,
-        gender: "male"
+        id: 0,
+        name: "Star Wars = The new one",
+        score: 1
     },
     {
-        id: "2",
-        name: "Jisu",
-        age: 18,
-        gender: "female"
+        id: 1,
+        name: "Avengers - The new one",
+        score: 8
     },
     {
-        id: "3",
-        name: "Japan Guy",
-        age: 18,
-        gender: "male"
+        id: 2,
+        name: "The Godfather I",
+        score: 99
     },
     {
-        id: "4",
-        name: "Daal",
-        age: 18,
-        gender: "male"
+        id: 3,
+        name: "Logan",
+        score: 2
     },
-    {
-        id: "5",
-        name: "JD",
-        age: 18,
-        gender: "male"
-    },
-    {
-        id: "6",
-        name: "moondaddi",
-        age: 18,
-        gender: "male"
-    },
-    {
-        id: "7",
-        name: "flynn",
-        age: 18,
-        gender: "male"
-    }
 ];
+
+export const getMovies = () => movies;
 
 export const getById = id => {
     // filter는 모든 대상을 거친 뒤 해당 조건에 맞는 걸 return 한다.
-    const filteredPeople = people.filter(person => person.id === String(id));
+    const filteredPeople = movies.filter(movie => movie.id === String(id));
     return filteredPeople[0];
-}
+};
+
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== String(id))
+    if(movies.length > cleanedMovies.length) {
+        movie = cleanedMovies;
+        return true;
+    } else {
+        return false;
+    }
+};
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
+};
+
 

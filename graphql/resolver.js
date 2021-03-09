548e5a6abbe87bc522d864c75f49461e43729753
castle.jaemin <ehdrud1332@gmail.com>
2021-03-09 13:27:00 +0900

    2.1 Extending the Schema

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,7 +1,13 @@
 //Resolver 는 query를 resolve(해결) 한다.
+const jaemin = {
+    name: 'seong',
+    age: 25,
+    gender: "male"
+}
+
 const resolvers = {
     Query: {
-        name: () => "jaemin"
+        person: () => jaemin
     }
 }
 
